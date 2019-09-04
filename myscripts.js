69cd436a9fde662482cbe4814136d915b7c5873f
AaronParkin <54840287+AaronParkin@users.noreply.github.com>
2019-09-04 13:22:56 +0100

    Create myscripts.js

---
+++ myscripts.js
@@ -0,0 +1,8 @@
+ <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js>
+ </script>
+ <script type="text/javascript">
+ 	function showToast() {
+ 		 $('.toast').toast('show');
+ 	}
+ </script>
+ <body onload="showToast()"> or <button onclick="showToast(); return false;"> 
